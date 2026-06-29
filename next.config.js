4d2d7427735e917b000742d1c669987713e2e4d3
wlt <2497189690@qq.com>
2026-06-30 01:13:19 +0800

    修改配置

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["uncrypto", "isows"],
+  output: "export",
+  images: { unoptimized: true },
   webpack(config) {
     config.resolve.alias['@react-native-async-storage/async-storage'] = false;
     return config;
